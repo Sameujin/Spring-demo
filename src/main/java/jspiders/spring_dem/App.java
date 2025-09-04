3f15361f6b666a6a61fcc01a10474bf3f505745b
Sameujin <jsameujene@gmail.com>
2025-09-04 17:12:36 +0530

    clear

--- src/main/java/jspiders/spring_dem/App.java
+++ src/main/java/jspiders/spring_dem/App.java
@@ -1,10 +1,16 @@
 package jspiders.spring_dem;
 
-/**
- * Hello world!
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import jspiders.spring_dem.dto.Student;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/beans.xml");
+
+        System.out.println("IoC Container created");
+        System.out.println(context.getBean("student1", Student.class));
+        System.out.println(context.getBean("student2", Student.class));
     }
 }
---
+++ src/main/java/jspiders/spring_dem/dto/Student.java
@@ -0,0 +1,11 @@
+package jspiders.spring_dem.dto;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private int studentId;
+    private String name;
+    private String email;
+    private String contact;
+}
